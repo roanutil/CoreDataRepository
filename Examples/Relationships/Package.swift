db7029c5a3cc3a7793826b0b2f6ebeaf862e0610
Andrew Roan <roanutil@users.noreply.github.com>
2022-08-01 22:16:24 -0500

    WIP
    add-examples-for-relationships

---
+++ Examples/Relationships/Package.swift
@@ -0,0 +1,7 @@
+// swift-tools-version:5.6
+
+import PackageDescription
+
+var package: Package = Package(
+    name: "ignore-me"
+)
---
+++ Examples/Relationships/RelationshipsExample/RelationshipsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelationshipsExampleApp.swift
+//  RelationshipsExample
+//
+//  Created by andrew on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RelationshipsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
