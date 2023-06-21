eeeb2960dcfffe0e557ed348c7059105c51f9544
Andrew Roan <roanutil@users.noreply.github.com>
2023-06-21 08:52:50 -0500

    WIP: Add doc comments
    3.0-preview

--- Sources/CoreDataRepository/CoreDataBatchError.swift
+++ Sources/CoreDataRepository/CoreDataBatchError.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/// An error that models the failure of a single item in a batch operation.
 public struct CoreDataBatchError<T>: Error {
     public let item: T
     public let error: CoreDataError
--- Sources/CoreDataRepository/CoreDataRepository.swift
+++ Sources/CoreDataRepository/CoreDataRepository.swift
@@ -12,16 +12,10 @@ import Foundation
 
 /// A CoreData repository with typical create, read, update, and delete endpoints
 public final class CoreDataRepository {
-    // MARK: Properties
 
-    /// CoreData context the repository uses
+    /// CoreData context the repository uses. A child or 'scratch' context is usually created from this context for work to be performed in.
     public let context: NSManagedObjectContext
 
-    // MARK: Init
-
-    /// Initializes a CRUDRepository
-    /// - Parameters
-    ///     - context: NSManagedObjectContext
     public init(context: NSManagedObjectContext) {
         self.context = context
     }
