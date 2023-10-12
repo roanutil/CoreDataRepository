4b344af4db99f7a14d3037897a976cf74f4fe758
Andrew Roan <roanutil@users.noreply.github.com>
2023-10-11 23:48:47 -0500

    Fix FetchSubscription
    3.0-preview-async-deadlock

--- Sources/CoreDataRepository/Internal/FetchSubscription.swift
+++ Sources/CoreDataRepository/Internal/FetchSubscription.swift
@@ -6,15 +6,27 @@
 //
 // Copyright © 2023 Andrew Roan
 
+import Foundation
+
 /// Subscription provider that sends updates when a fetch request changes
 final class FetchSubscription<Model: UnmanagedModel>: Subscription<[Model], Model.ManagedModel, Model.ManagedModel> {
     override func fetch() {
-        frc.managedObjectContext.perform { [weak self, frc, subject] in
-            guard let fetchedObjects = frc.fetchedObjects else {
+        frc.managedObjectContext.perform { [weak self, frc, subject, request] in
+            guard frc.fetchedObjects != nil else {
                 self?.start()
                 return
             }
-            subject.send(fetchedObjects.map(Model.init(managed:)))
+
+            do {
+                let result = try frc.managedObjectContext.fetch(request)
+                subject.send(result.map(Model.init(managed:)))
+            } catch let error as CocoaError {
+                subject.send(completion: .failure(.cocoa(error)))
+                return
+            } catch {
+                subject.send(completion: .failure(.unknown(error as NSError)))
+                return
+            }
         }
     }
 
