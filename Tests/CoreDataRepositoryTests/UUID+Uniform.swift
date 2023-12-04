328426d1446982551dcfb60081ac610b5f29b2aa
Andrew Roan <roanutil@users.noreply.github.com>
2023-12-03 19:24:30 -0600

    Add a uniform UUID initializer to tests for making UUIDs that contain a single character repeated
    3.0-preview

---
+++ Tests/CoreDataRepositoryTests/UUID+Uniform.swift
@@ -0,0 +1,19 @@
+// UUID+Uniform.swift
+// CoreDataRepository
+//
+//
+// MIT License
+//
+// Copyright © 2023 Andrew Roan
+
+import Foundation
+
+extension UUID {
+    /// BA7097EA-70FD-4595-9268-6D415A222C7B
+    init(uniform character: Character) {
+        let x8 = String(repeating: character, count: 8)
+        let x4 = String(repeating: character, count: 4)
+        let x12 = String(repeating: character, count: 12)
+        self.init(uuidString: "\(x8)-\(x4)-\(x4)-\(x4)-\(x12)")!
+    }
+}
